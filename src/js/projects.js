d930d564871d6df2e380dda5a18025a8bf673ba6
Dominic van Almsick <dominicvana@gmail.com>
2020-10-21 18:55:05 +0100

    projects page (very) basic structure added

--- src/js/projects.js
+++ src/js/projects.js
@@ -4,6 +4,7 @@ import '../styles/resets.scss';
 import '../styles/_style-guide.scss';
 import '../styles/shared.scss';
 import '../styles/areas.scss';
+import '../styles/projects.scss';
 
 import khanAcademyLogo from '../media/khan-academy-logo.svg';
 import freeCodeCampLogo from '../media/freecodecamp-vector-logo.svg';
