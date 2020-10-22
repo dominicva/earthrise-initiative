e0ed16e37e5feb5554a19a3b064576e466fb3376
Dominic van Almsick <dominicvana@gmail.com>
2020-10-22 07:49:15 +0100

    Refactor: menu toggle handling

--- src/js/project-areas/headerNavModal.js
+++ src/js/project-areas/headerNavModal.js
@@ -1,15 +1,25 @@
+// refactor: this file shouldn't be in this directory.
+// beware of dependencies
 export let isMenuModalOpen = false;
+
+const openMenuModal = (button, nav) => {
+  button.textContent = 'close';
+  nav.classList.add('open');
+};
+
+const closeMenuModal = (button, nav) => {
+  button.textContent = 'menu';
+  nav.classList.remove('open');
+};
 
 export const menuToggleBtnHandler = () => {
   const menuToggleBtn = document.getElementById('header__menu-btn');
   const headerNav = document.querySelector('.header__nav');
   if (!isMenuModalOpen) {
-    menuToggleBtn.innerHTML = 'close';
-    headerNav.classList.add('open');
+    openMenuModal(menuToggleBtn, headerNav);
     isMenuModalOpen = true;
   } else {
-    menuToggleBtn.innerHTML = 'menu';
-    headerNav.classList.remove('open');
+    closeMenuModal(menuToggleBtn, headerNav);
     isMenuModalOpen = false;
   }
 };
