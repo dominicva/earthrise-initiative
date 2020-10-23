e630368be9543a9806cf73d9b9ba9290876c9c7e
Dominic van Almsick <dominicvana@gmail.com>
2020-10-23 09:17:26 +0100

    Feat: dynamically set explore projects attributes

--- src/js/heroNav.js
+++ src/js/heroNav.js
@@ -1,42 +1,56 @@
 /* eslint-disable indent */
-export const navItemHoverHandler = (event) => {
+export const navItemHoverHandler = event => {
   event.target.lastElementChild.classList.add('active');
 };
 
-export const navItemExitHoverHandler = (event) => {
+export const navItemExitHoverHandler = event => {
   event.target.lastElementChild.classList.remove('active');
 };
 
-export const navItemClickHandler = (event) => {
+const setBtnAttribute = link => {
+  const exploreProjectsBtn = document.querySelector(
+    '.explore-projects-btn.container'
+  );
+
+  for (const child of exploreProjectsBtn.children) {
+    child.setAttribute('href', link);
+  }
+};
+
+export const navItemClickHandler = event => {
   const areaTitle = document.getElementById('area-title');
   const areasQuestion = document.getElementById('area-question');
-  const exploreProjectsBtn = document.getElementById('explore-projects-btn');
+  // const exploreProjectsBtn = document.getElementById('explore-projects-btn');
 
   switch (event.target.innerHTML) {
     case 'education':
       areaTitle.innerHTML = 'education';
       areasQuestion.innerHTML =
         'How can we provide everyone with a world-class education?';
-      exploreProjectsBtn.setAttribute('href', './education.html');
+      // exploreProjectsBtn.setAttribute('href', './education.html');
+      setBtnAttribute('./education.html');
       break;
 
     case 'mental health':
       areaTitle.innerHTML = 'mental health';
       areasQuestion.innerHTML =
         'How can we combine science and awareness to end mental suffering?';
-      exploreProjectsBtn.setAttribute('href', './mental-health.html');
+      // exploreProjectsBtn.setAttribute('href', './mental-health.html');
+      setBtnAttribute('./mental-health.html');
       break;
 
     case 'basic income':
       areaTitle.innerHTML = 'basic income';
       areasQuestion.innerHTML =
         'What if we could free ourselves from existential wage-reliance?';
+      setBtnAttribute('./basic-income.html');
       break;
 
     case 'animal welfare':
       areaTitle.innerHTML = 'animal welfare';
       areasQuestion.innerHTML =
         'How can we bring an end to factory farming of non-human animals?';
+      setBtnAttribute('./animal-welfare.html');
       break;
   }
 };
