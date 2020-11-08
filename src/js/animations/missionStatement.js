aefdb8406bb5f8c18fc3514503a0a1762ba10b2c
Dominic van Almsick <dominicvana@gmail.com>
2020-11-08 16:51:06 -0500

    Feat: transition on page load added to mission statement

--- src/js/animations/missionStatement.js
+++ src/js/animations/missionStatement.js
@@ -1,17 +1,22 @@
-export const missionStatementAnimation = () => {
+export const missionStatementAnimation = async () => {
+  const missionStatement = document.querySelector('.hero__mission-statement');
   let i = 0;
   const text = 'global shift towards equality of consideration for all';
-  const speed = 50;
+  const speed = 34;
 
-  function typingEffect() {
+  async function typingEffect() {
     if (i < text.length) {
-      document.querySelector(
-        '.hero__mission-statement'
-      ).textContent += text.charAt(i);
+      missionStatement.textContent += text.charAt(i);
       i++;
       setTimeout(typingEffect, speed);
     }
   }
 
-  typingEffect();
+  function transform(pxVal = 0, transition = 'transform 1.75s ease-out') {
+    missionStatement.style.transform = `translateY(${pxVal}px`;
+    missionStatement.style.transition = transition;
+  }
+
+  const delay = 2600;
+  await typingEffect().then(setTimeout(transform, delay));
 };
